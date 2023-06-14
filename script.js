0fee0ffe014292281aff4d53445a69f97e90613d
CLEVER TECH <ebinumclever28@gmail.com>
2023-06-14 15:05:01 -0700

    another commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// let dateEl = new Date()
+
+// console.log(dateEl);
+
+// const timer = document.querySelector(".time")
+
+// function getDate(){
+//     let date = new Date()
+//     // let val = date.toLocaleDateString()
+//     let val = date.toLocaleTimeString()
+//     // console.log(val);
+//     // console.log(vall);
+
+//     timer.innerHTML = val
+
+// }
+
+// setInterval(getDate, 1000)
+
+// let val = document
+// console.log(val);
+
+// document.addEventListener ("DOMContentLoaded", display)
+
+// function display(){
+//     document.querySelector(`#${data}`)
+
+// }
+const imgEl = document.querySelector(".u-person")
+document.querySelector("").addEventListener("click", function(e){
+if(e.target.classList.contains("img")){
+    alert("thid is")
+}
+})
